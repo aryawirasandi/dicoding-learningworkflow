548cdb1ca4f27a0f0aa9f58882115b83b34b0343
Arya Wirasandi <aryawirasandi7153@gmail.com>
2020-05-16 23:03:57 +0700

    add search bar component

---
+++ src/script/components/SearchBar.js
@@ -0,0 +1,17 @@
+class SearchBar extends HTMLElement {
+    
+    connectedCallback(){
+        this.render();
+    }
+
+    render() {
+        this.innerHTML = `
+        <div id="search-container" class="search-container">
+            <input placeholder="Search football club" id="searchElement" type="search">
+            <button id="searchButtonElement" type="submit">Search</button>
+        </div>
+        `
+    }
+}
+
+customElements.define('search-bar', SearchBar);
