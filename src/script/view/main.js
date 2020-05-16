4973e2e3cd440611c5ec554a6ca614743dc3c7e5
Arya Wirasandi <aryawirasandi7153@gmail.com>
2020-05-16 23:04:37 +0700

    change main js render result with component

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,4 +1,5 @@
 import DataSource from '../data/data-source.js';
+import '../components/ClubList.js'
 var main = () => {
     const searchElement = document.querySelector("#searchElement");
     const buttonSearchElement = document.querySelector("#searchButtonElement");
@@ -16,23 +17,9 @@ var main = () => {
   
   
     const renderResult = (results) => {
-        clubListElement.innerHTML = "";
-        results.forEach(function (club) {
-
-            const {name, fanArt, description} = club;
-  
-            const clubElement = document.createElement("div");
-            clubElement.setAttribute("class", "club");
-  
-  
-            clubElement.innerHTML = `
-                <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-                <div class="club-info">
-                    <h2>${name}</h2>
-                    <p>${description}</p>
-                </div>`;
-            clubListElement.appendChild(clubElement);
-        })
+        const ClubList = document.createElement('club-list');
+        ClubList.items = results;
+        clubListElement.appendChild(ClubList);
     };
   
   
